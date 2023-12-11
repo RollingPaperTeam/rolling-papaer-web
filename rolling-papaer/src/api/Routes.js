0c0609246ee1200251e29a8efca02e2c62a1e550
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-12-11 16:08:49 +0900

    Feat: 추가 Api Route 경로
      MESSAGES
      RECIPIENTS
      BACKGROUND_IMAGES
      PROFILE_IMAGES

---
+++ rolling-papaer/src/api/Routes.js
@@ -0,0 +1,9 @@
+const BASE_API_URL = "rolling-api.vercel.app";
+const BASE_API_URL_TEAM = `${BASE_API_URL}/2-6`
+
+const ROUTES = {
+  MESSAGES: `${BASE_API_URL_TEAM}/messages`, //get put push delete
+  RECIPIENTS: `${BASE_API_URL_TEAM}/recipients`, //get put push delete
+  BACKGROUND_IMAGES: `${BASE_API_URL}/background-images/`, // get
+  PROFILE_IMAGES: `${BASE_API_URL}/profile_images/` //get
+};
