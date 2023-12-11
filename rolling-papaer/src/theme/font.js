ca6bfcd18c6fce8fbc1e29831055ac89a1a523a7
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-12-11 16:09:58 +0900

    Feat: 추가 Font 객체

---
+++ rolling-papaer/src/theme/font.js
@@ -0,0 +1,100 @@
+const FONTS = {
+  FONT_28_BOLD: `
+	  font-family: Pretendard;
+	  font-size: 1.75rem;
+	  font-weight: 700;
+	  line-height: 2.625rem;
+	  letter-spacing: -0.0175rem;
+	`,
+  FONT_24_BOLD: `
+	  font-family: Pretendard;
+	  font-size: 1.5rem;
+	  font-weight: 700;
+	  line-height: 2.25rem;
+	  letter-spacing: -0.015rem;
+	`,
+  FONT_24_REGULAR: `
+	  font-family: Pretendard;
+	  font-size: 1.5rem;
+	  font-weight: 400;
+	  line-height: 2.25rem;
+	  letter-spacing: -0.015rem;
+	`,
+  FONT_20_BOLD: `
+	  font-family: Pretendard;
+	  font-size: 1.25rem;
+	  font-weight: 700;
+	  line-height: 1.875rem;
+	  letter-spacing: -0.0125rem;
+	`,
+  FONT_20_REGULAR: `
+	  font-family: Pretendard;
+	  font-size: 1.25rem;
+	  font-weight: 400;
+	  line-height: 1.875rem;
+	  letter-spacing: -0.0125rem;
+	`,
+  FONT_18_BOLD: `
+	  font-family: Pretendard;
+	  font-size: 1.125rem;
+	  font-weight: 700;
+	  line-height: 1.75rem;
+	  letter-spacing: -0.01125rem;
+	`,
+  FONT_18_REGULAR: `
+	  font-family: Pretendard;
+	  font-size: 1.125rem;
+	  font-weight: 400;
+	  line-height: 1.75rem;
+	  letter-spacing: -0.01125rem;
+	`,
+  FONT_16_BOLD: `
+	  font-family: Pretendard;
+	  font-size: 1rem;
+	  font-weight: 700;
+	  line-height: 1.625rem;
+	  letter-spacing: -0.01rem;
+	`,
+  FONT_16_REGULAR: `
+	  font-family: Pretendard;
+	  font-size: 1rem;
+	  font-weight: 400;
+	  line-height: 1.625rem;
+	  letter-spacing: -0.01rem;
+	`,
+  FONT_15_REGULAR: `
+	  font-family: Pretendard;
+	  font-size: 0.9375rem;
+	  font-weight: 400;
+	  line-height: 1.375rem;
+	  letter-spacing: -0.00938rem;
+	`,
+  FONT_15_BOLD: `
+	  font-family: Pretendard;
+	  font-size: 0.9375rem;
+	  font-weight: 700;
+	  line-height: 1.375rem;
+	  letter-spacing: -0.00938rem;
+	`,
+  FONT_14_REGULAR: `
+	  font-family: Pretendard;
+	  font-size: 0.875rem;
+	  font-weight: 400;
+	  line-height: 1.25rem;
+	  letter-spacing: -0.00438rem;
+	`,
+  FONT_14_BOLD: `
+	  font-family: Pretendard;
+	  font-size: 0.875rem;
+	  font-weight: 700;
+	  line-height: 1.25rem;
+	  letter-spacing: -0.00438rem;
+	`,
+  FONT_12_REGULAR: `
+	  font-family: Pretendard;
+	  font-size: 0.75rem;
+	  font-weight: 400;
+	  line-height: 1.125rem;
+	  letter-spacing: -0.00375rem;
+	`,
+};
