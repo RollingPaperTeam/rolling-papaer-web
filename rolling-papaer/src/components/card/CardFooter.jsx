5069dbf2ff7e784ef007c70fd321c16f110276f3
nakyoung98 <jay3925@cau.ac.kr>
2023-12-14 15:57:45 +0900

    Feat: 수정 CardFooter 컴포넌트
    Provider로 전달된 값을 사용하도록 함
    이와 연계하여 createAt을 year/month/day 문자열로 변환하는 util 파일 함께 생성
    
    rel: #13 , #2

--- rolling-papaer/src/components/card/CardFooter.jsx
+++ rolling-papaer/src/components/card/CardFooter.jsx
@@ -1,13 +1,17 @@
 import styled from "styled-components";
 import FONTS from "../../theme/font";
 import { THEME_LIGHT_COLOR } from "../../theme/color";
+import { useCardContextValue } from "./CardProvider";
+import { createdAtToDate } from "../../utils/timeformatUtil";
 
 const CardFooterBlock = styled.p`
   color: ${THEME_LIGHT_COLOR.gray4};
   ${FONTS.FONT_12_REGULAR}
 `;
 
-function CardFooter({ date = "2023/07/08" }) {
+function CardFooter() {
+  const {createdAt} = useCardContextValue();
+  const date = createdAtToDate(createdAt);
   return <CardFooterBlock>{date}</CardFooterBlock>;
 }
 
