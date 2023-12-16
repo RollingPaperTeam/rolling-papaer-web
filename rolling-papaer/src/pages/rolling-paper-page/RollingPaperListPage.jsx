e0cec4c9087629f717d24ed1dae1246c8bca6626
seongseonko <kymgok1001@naver.com>
2023-12-16 15:33:50 +0900

    [feat] RollingPaperListPage 추가

--- rolling-papaer/src/pages/rolling-paper-page/RollingPaperListPage.jsx
+++ rolling-papaer/src/pages/rolling-paper-page/RollingPaperListPage.jsx
@@ -4,9 +4,12 @@ function RollingPaperListPage() {
   return (
     <>
       <nav></nav>
-      <CardSection children="인기 롤링 페이퍼 🔥" />
-      <CardSection children="최근에 만든 롤링 페이퍼 ⭐️️" />
-      <button></button>
+      <CardSection children="인기 롤링 페이퍼 TOP10🔥" limit="10" like="like" />
+      <CardSection
+        children="최근에 만든 롤링 페이퍼 TOP10⭐️️"
+        limit="10"
+        like=""
+      />
     </>
   );
 }
