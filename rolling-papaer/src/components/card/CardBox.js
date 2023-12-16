ffb3fc4c5a7144b4a66716935d5e3600b62a2491
seongseonko <kymgok1001@naver.com>
2023-12-16 16:14:05 +0900

    [Refactor] Cardbox 리팩토링

--- rolling-papaer/src/components/card/CardBox.js
+++ rolling-papaer/src/components/card/CardBox.js
@@ -4,26 +4,30 @@ import blue from "../../static/bg-blue.png";
 import beige from "../../static/bg-beige.png";
 import green from "../../static/bg-green.png";
 
+const BackgroundColor = {
+  purple: purple,
+  blue: blue,
+  beige: beige,
+  green: green,
+  default: purple,
+};
+
 const CardBox = styled.div`
   width: 27.5rem;
   height: 26rem;
-  ${({ $backgroundColor, $backgroundIMG }) =>
-    $backgroundIMG === null
-      ? $backgroundColor === "purple"
-        ? `background-image: url(${purple})`
-        : $backgroundColor === "beige"
-        ? `background-image: url(${beige})`
-        : $backgroundColor === "blue"
-        ? `background-image: url(${blue})`
-        : $backgroundColor === "green"
-        ? `background-image: url(${green})`
-        : `background: linear-gradient(180deg, rgba(0, 0, 0, 0.54) 0%, rgba(0, 0, 0, 0.54) 100%) , url(${$backgroundIMG})`
-      : `background: linear-gradient(180deg, rgba(0, 0, 0, 0.54) 0%, rgba(0, 0, 0, 0.54) 100%) , url(${$backgroundIMG})`};
+
+  ${({ $backgroundIMG, $backgroundColor }) =>
+    $backgroundIMG
+      ? `background: linear-gradient(180deg, rgba(0, 0, 0, 0.54) 0%, rgba(0, 0, 0, 0.54) 100%), url(${$backgroundIMG});`
+      : `background-image: url(${
+          BackgroundColor[$backgroundColor] ?? BackgroundColor["default"]
+        });
+     `}
+
   background-position: center;
   background-size: 100% 26rem;
   border-radius: 1.6rem;
   border: 1px solid rgba(0, 0, 0, 0.1);
   box-shadow: 0 0.2rem 1.2rem 0 rgba(0, 0, 0, 0.08);
 `;
-
 export default CardBox;
