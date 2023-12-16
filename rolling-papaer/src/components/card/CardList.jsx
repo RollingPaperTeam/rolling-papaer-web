79c323b7a33956d824c5da39e5123c722e23ff8c
seongseonko <kymgok1001@naver.com>
2023-12-16 15:34:53 +0900

    [feat] CardList 추가

--- rolling-papaer/src/components/card/CardList.jsx
+++ rolling-papaer/src/components/card/CardList.jsx
@@ -1,35 +1,113 @@
 import styled from "styled-components";
-import mockData from "../../static/mock-recipient-list.json";
 import FONTS from "../../theme/font";
-import THEME_LIGHT_COLOR from "../../theme/color";
-const CardBox = styled.div`
-  width: 17.1875rem;
-  height: 16.25rem;
+import CardBox from "./CardBox";
+import RecentProfile from "./RecentProfile";
+const Wrapper = styled.div`
+  padding: 3rem 2.4rem 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 4.3rem;
 `;
-const CardName = styled.span`
+const ReactionBox = styled.div``;
+const Line = styled.span`
+  display: block;
+  width: 22.7rem;
+  height: 0.1rem;
+  background: rgba(0, 0, 0, 0.12);
+`;
+const CardContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+`;
+const CardContentImgContainer = styled.div`
+  display: flex;
+  align-items: center;
+  height: 2.8rem;
+
+  img {
+    width: 2.8rem;
+    height: 2.8rem;
+    border-radius: 5rem;
+    border: 0.15rem solid var(--white);
+  }
+  img:nth-of-type(2){
+    position: relative;
+    right: 1.2rem;
+  }
+  img:nth-of-type(3){
+    position: relative;
+    right: 2.4rem;
+  }
+  }
+`;
+const CardEmoji = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 0.8rem;
+
+  p {
+    display: inline;
+  }
+`;
+const RecentCount = styled.div`
+  border-radius: 3rem;
+  background: var(--white);
+  display: flex;
+  padding: 0.5rem 0.6rem;
+  align-items: flex-start;
+  gap: 1rem;
+  color: var(--gray5);
+  ${FONTS.FONT_12_REGULAR}
+  position: relative;
+  right: 3.6rem;
+`;
+const Name = styled.span`
   ${FONTS.FONT_24_BOLD}
+  color: var(${({ color }) => (color ? `--white` : `--gray9`)});
   display: -webkit-box;
   -webkit-box-orient: vertical;
   -webkit-line-clamp: 1;
   align-self: stretch;
 `;
-const CardMessageCount = styled.p`
+const RecentPerson = styled.p`
   ${FONTS.FONT_16_REGULAR}
-  color:${THEME_LIGHT_COLOR.gray7};
+  color:var(${({ color }) => (color ? `--gray2` : `--gray7`)});
+  margin: 0;
 `;
 
-//TODO
-function CardList() {
+function CardList({ cardData }) {
   return (
     <>
-      {mockData.results.map((result) => {
+      {cardData.map((result) => {
         return (
-          <CardBox key={result.id}>
-            <CardName>TO.{result.name}</CardName>
-
-            <CardMessageCount>
-              {result.messageCount}명이 작성했어요!
-            </CardMessageCount>
+          <CardBox
+            key={result.id}
+            $backgroundColor={result.backgroundColor}
+            $backgroundIMG={result.backgroundImageURL}
+          >
+            <Wrapper>
+              <CardContents>
+                <Name color={result.backgroundImageURL}>TO.{result.name}</Name>
+                <CardContentImgContainer>
+                  <RecentProfile result={result} />
+                  {result.messageCount - 3 > 0 && (
+                    <RecentCount>{`+${result.messageCount - 3}`}</RecentCount>
+                  )}
+                </CardContentImgContainer>
+                <RecentPerson color={result.backgroundImageURL}>
+                  {result.messageCount}명이 작성했어요!
+                </RecentPerson>
+              </CardContents>
+              <ReactionBox>
+                <Line></Line>
+                <CardEmoji>
+                  <p>d</p>
+                  <p>d</p>
+                  <p>d</p>
+                </CardEmoji>
+              </ReactionBox>
+            </Wrapper>
           </CardBox>
         );
       })}
