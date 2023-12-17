b8b939143d4c669f56a26eeee9510dbbec84ddf3
nakyoung98 <jay3925@cau.ac.kr>
2023-12-18 01:13:06 +0900

    Feat: 수정 EmojiBadge width값 조정
    min-width를 63px로 두되, count에 따라 길이가 늘어날 수 있도록 width는 fit-content로 둠

--- rolling-papaer/src/components/badge/EmojiBadge.jsx
+++ rolling-papaer/src/components/badge/EmojiBadge.jsx
@@ -1,20 +1,33 @@
 import styled from "styled-components";
 import { THEME_LIGHT_COLOR } from "../../theme/color";
+import FONTS from "../../theme/font";
 
 const EmojiBadgeBlock = styled.div`
-  width: 42px;
-  height: 20px;
+  min-width: 63px;
+  width: fit-content;
+  height: 36px;
   padding: 8px 12px;
-  border-radius: 9999px;
+  border-radius: 32px;
   background-color: rgba(0, 0, 0, 0.54);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 2px;
+
+  ${FONTS.FONT_16_REGULAR}
+
+  span{
+    margin: 0;
+    color: ${THEME_LIGHT_COLOR.white};
+  }
 `;
 
 function EmojiBadge({ emoji, count }) {
   return (
     <EmojiBadgeBlock>
-      <p>
-        {emoji} {count}
-      </p>
+      <span>{emoji}</span>
+      <span>{count}</span>
     </EmojiBadgeBlock>
   );
 }
