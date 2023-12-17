691079bd2a58a83da65587f149abb1b3bc272b3b
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2023-12-17 21:33:55 +0900

    Feat: 추가 Provider, GlobalStyle
    Provider와 GlobalStyle로 페이지 배경색 변경

--- rolling-papaer/src/Main.js
+++ rolling-papaer/src/Main.js
@@ -6,24 +6,29 @@ import InputMessageContentPage from "./pages/send-message-page/InputMessageConte
 import PickReceiverPage from "./pages/send-message-page/PickReceiverPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import App from "./App";
+import { ColorProvider } from "./components/option/OptionContext";
+import OptionGlobalStyle from "./components/option/OptionGlobalStyle";
 
 function Main() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<MainPage />} />
-          <Route path="list" element={<RollingPaperListPage />} />
-          <Route path="post">
-            <Route index element={<PickReceiverPage />} />
-            <Route path=":id" element={<RollingPaperPage />}>
-              <Route path="edit" element={<RollingPaperPage />} />
-              <Route path="message" element={<InputMessageContentPage />} />
+      <ColorProvider>
+        <OptionGlobalStyle />
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<MainPage />} />
+            <Route path="list" element={<RollingPaperListPage />} />
+            <Route path="post">
+              <Route index element={<PickReceiverPage />} />
+              <Route path=":id" element={<RollingPaperPage />}>
+                <Route path="edit" element={<RollingPaperPage />} />
+                <Route path="message" element={<InputMessageContentPage />} />
+              </Route>
             </Route>
           </Route>
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </ColorProvider>
     </BrowserRouter>
   );
 }
