d1b94107dec7412d72885da899b508be2a3a63fc
nakyoung98 <jay3925@cau.ac.kr>
2023-12-18 01:15:01 +0900

    Feat: 추가 EmojiBadgeList 컴포넌트
    EmojiBadge list가 담긴 EmojiBadgeList 컴포넌트
    
    function EmojiBadgeList({
      emojiList, // 표시할 emoji list
      maxEmojisPerLine = 3, //기본적으로 한 줄에 3개를 표시함
      isAscending = false, // 오름차순,내림차순 설정
      showAll = false, // list 범위가 한 줄 이상일 때 숨겨진 나머지 줄도 모두 표시할 것인지
    }
    
    rel #21

---
+++ rolling-papaer/src/components/badge/EmojiBadgeList.jsx
@@ -0,0 +1,46 @@
+import { useMemo } from "react";
+import styled from "styled-components";
+import EmojiBadge from "./EmojiBadge";
+
+const EmojiBadgeListBlock = styled.article`
+  width: fit-content;
+  display: grid;
+
+  grid-template-columns: repeat(${(props) => props.$columnLength}, 1fr);
+  gap: 8px;
+`;
+
+function EmojiBadgeList({
+  emojiList,
+  maxEmojisPerLine = 3,
+  isAscending = false,
+  showAll = false,
+}) {
+  const sortedEmojiList = useSortedEmojiList(
+    emojiList,
+    showAll ? emojiList.length : maxEmojisPerLine,
+    isAscending
+  );
+
+  return (
+    <EmojiBadgeListBlock $columnLength={maxEmojisPerLine}>
+      {sortedEmojiList.map((item) => (
+        <EmojiBadge id={item.id} emoji={item.emoji} count={item.count} />
+      ))}
+    </EmojiBadgeListBlock>
+  );
+}
+
+const useSortedEmojiList = (emojiList, maxShowEmojiCount, isAscending) => {
+  return useMemo(() => {
+    const copyEmojiList = [...emojiList];
+    if (isAscending) {
+      copyEmojiList.sort((emoji1, emoji2) => emoji1.count - emoji2.count);
+    } else {
+      copyEmojiList.sort((emoji1, emoji2) => emoji2.count - emoji1.count);
+    }
+    return copyEmojiList.slice(0, maxShowEmojiCount);
+  }, [emojiList, maxShowEmojiCount, isAscending]);
+};
+
+export default EmojiBadgeList;
