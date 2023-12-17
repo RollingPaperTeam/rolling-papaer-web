cf3a3d0a58adcf71c3b2b65ec25dd44800fa2c6f
mijin0928 <94034865+mijin0928@users.noreply.github.com>
2023-12-17 22:02:37 +0900

    Feat: 추가 테스트파일
    해당하는 페이지만 배경색 적용하는 테스트 파일 생성

---
+++ rolling-papaer/src/components/option/Test.jsx
@@ -0,0 +1,11 @@
+import OptionGlobalStyle from "./OptionGlobalStyle";
+
+function Test() {
+  return (
+    <>
+      <OptionGlobalStyle />
+      <div>테스트</div>
+    </>
+  );
+}
+export default Test;
