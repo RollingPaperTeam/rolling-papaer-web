a06d2823f4f7af2d8fda752152244da476d3cd7a
seongseonko <kymgok1001@naver.com>
2023-12-19 21:02:04 +0900

    [feat] list 페이지 Card 리스트 Link화
    지속적인 day8-고성선 브랜치 생성으로 42번줄 conflict 우려됨

--- rolling-papaer/src/components/card/CardList.jsx
+++ rolling-papaer/src/components/card/CardList.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import FONTS from "../../theme/font";
 import CardBox from "./CardBox";
 import RecentProfile from "./RecentProfile";
+import { Link } from "react-router-dom";
 const Wrapper = styled.div`
   padding: 3rem 2.4rem 2rem;
   display: flex;
@@ -31,14 +32,13 @@ const CardContentImgContainer = styled.div`
     border-radius: 5rem;
     border: 0.15rem solid var(--white);
   }
-  img:nth-of-type(2){
+  img:nth-of-type(2) {
     position: relative;
     right: 1.2rem;
   }
-  img:nth-of-type(3){
+  img:nth-of-type(3) {
     position: relative;
     right: 2.4rem;
-  }
   }
 `;
 const CardEmoji = styled.div`
@@ -81,34 +81,37 @@ function CardList({ cardData }) {
     <>
       {cardData.map((result) => {
         return (
-          <CardBox
-            key={result.id}
-            $backgroundColor={result.backgroundColor}
-            $backgroundIMG={result.backgroundImageURL}
-          >
-            <Wrapper>
-              <CardContents>
-                <Name color={result.backgroundImageURL}>TO.{result.name}</Name>
-                <CardContentImgContainer>
-                  <RecentProfile result={result} />
-                  {result.messageCount - 3 > 0 && (
-                    <RecentCount>{`+${result.messageCount - 3}`}</RecentCount>
-                  )}
-                </CardContentImgContainer>
-                <RecentPerson color={result.backgroundImageURL}>
-                  {result.messageCount}명이 작성했어요!
-                </RecentPerson>
-              </CardContents>
-              <ReactionBox>
-                <Line></Line>
-                <CardEmoji>
-                  <p>d</p>
-                  <p>d</p>
-                  <p>d</p>
-                </CardEmoji>
-              </ReactionBox>
-            </Wrapper>
-          </CardBox>
+          <Link to={`${result.id}`} key={result.id}>
+            <CardBox
+              $backgroundColor={result.backgroundColor}
+              $backgroundIMG={result.backgroundImageURL}
+            >
+              <Wrapper>
+                <CardContents>
+                  <Name color={result.backgroundImageURL}>
+                    TO.{result.name}
+                  </Name>
+                  <CardContentImgContainer>
+                    <RecentProfile result={result} />
+                    {result.messageCount - 3 > 0 && (
+                      <RecentCount>{`+${result.messageCount - 3}`}</RecentCount>
+                    )}
+                  </CardContentImgContainer>
+                  <RecentPerson color={result.backgroundImageURL}>
+                    {result.messageCount}명이 작성했어요!
+                  </RecentPerson>
+                </CardContents>
+                <ReactionBox>
+                  <Line></Line>
+                  <CardEmoji>
+                    <p>d</p>
+                    <p>d</p>
+                    <p>d</p>
+                  </CardEmoji>
+                </ReactionBox>
+              </Wrapper>
+            </CardBox>
+          </Link>
         );
       })}
     </>
