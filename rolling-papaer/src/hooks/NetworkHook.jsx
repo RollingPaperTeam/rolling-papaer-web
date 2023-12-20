cdf4a81e4dc0ab40224446c1a4bad077e46f25b8
nakyoung98 <jay3925@cau.ac.kr>
2023-12-21 04:03:14 +0900

    add: 보완 useAsync 훅
    wrappedFunction또한 useCallback을 사용하여 메모이제이션

--- rolling-papaer/src/hooks/NetworkHook.jsx
+++ rolling-papaer/src/hooks/NetworkHook.jsx
@@ -1,22 +1,25 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 const useAsync = (asyncFunction) => {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
-  const wrappedFunction = async (...args) => {
-    try {
-      setIsLoading(true);
-      setIsError(false);
+  const wrappedFunction = useCallback(
+    async (...args) => {
+      try {
+        setIsLoading(true);
+        setIsError(false);
 
-      return await asyncFunction(...args);
-    } catch (e) {
-      setIsError(true);
-      return;
-    } finally {
-      setIsLoading(false);
-    }
-  };
+        return await asyncFunction(...args);
+      } catch (e) {
+        setIsError(true);
+        return;
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [asyncFunction]
+  );
 
   return [isLoading, isError, wrappedFunction];
 };
