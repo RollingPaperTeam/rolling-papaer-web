719179bde9c3952278947d221a035b6e11f8680d
nakyoung98 <jay3925@cau.ac.kr>
2023-12-20 22:27:46 +0900

    add: 보강 RollingPaperPage 컴포넌트
    top scroll되는 기능 구현을 위한 ScrollButton 및 Observer 를 통한 HeaderVisibilitySensor visibility 감시

--- rolling-papaer/src/pages/rolling-paper-page/RolingPaperPage.jsx
+++ rolling-papaer/src/pages/rolling-paper-page/RolingPaperPage.jsx
@@ -1,7 +1,91 @@
+import PostHeader from "../../components/header/PostHeader/PostHeader";
+import { useParams } from "react-router";
+import CardLazyGridContainer from "../../containers/CardLazyGridContainer";
+import styled from "styled-components";
+import Header from "../../components/header/Header";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const ScrollToTopButton = styled.button`
+  width: 100px;
+  height: 100px;
+  border-radius: 9999px;
+
+  position: fixed;
+  right: 50px;
+  bottom: 50px;
+
+  &:hover {
+    background-color: white;
+  }
+`;
+
+const RollingPaperPageBlock = styled.div`
+  position: relative;
+`;
+
+const HeaderVisibilitySensor = styled.div`
+  width: fit-content;
+  height: fit-content;
+`;
+
+const StyledHeader = styled(Header)`
+  width: 100%;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+`;
+
 function RollingPaperPage() {
+  const [isScrollToTopVisible, setIsScrollToTopVisible] = useState(false);
+  const header = useRef();
+
+  const { id } = useParams();
+
+  // 헤더가 화면에 보이지 않을 때 위로 스크롤 버튼을 노출
+  const handleScrollObserver = useCallback((entries) => {
+    const target = entries[0];
+    if (!target.isIntersecting) {
+      setIsScrollToTopVisible(true);
+    } else {
+      setIsScrollToTopVisible(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleScrollObserver, {
+      threshold: 1,
+    });
+
+    console.log(header.current);
+    if (header.current) {
+      observer.observe(header.current);
+    }
+
+    return () => observer.disconnect();
+  }, [handleScrollObserver]);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
-      <p>RollingPaperPage</p>
+      <RollingPaperPageBlock>
+        <HeaderVisibilitySensor ref={header}>
+          <StyledHeader ref={header} />
+        </HeaderVisibilitySensor>
+
+        <main>
+          <PostHeader></PostHeader>
+          <CardLazyGridContainer postId={id} />
+        </main>
+      </RollingPaperPageBlock>
+      {isScrollToTopVisible && (
+        <ScrollToTopButton onClick={scrollToTop}>UP</ScrollToTopButton>
+      )}
     </>
   );
 }
