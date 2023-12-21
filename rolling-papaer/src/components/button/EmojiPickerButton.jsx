987882c7e266a86f1d291f4024825d2ffe9b27ac
nakyoung98 <jay3925@cau.ac.kr>
2023-12-22 08:02:34 +0900

    Fix: EmojiPickerButton 수정

--- rolling-papaer/src/components/button/EmojiPickerButton.jsx
+++ rolling-papaer/src/components/button/EmojiPickerButton.jsx
@@ -1,9 +1,16 @@
 import EmojiPicker from "emoji-picker-react";
 import { useState } from "react";
 import styled from "styled-components";
+import ButtonStyle from "./ButtonStyle";
+import { AddIcon } from "./Button";
 
-const EmojiPickerButtonBlock = styled.article`
+
+const EmojiPickerBlock = styled.div`
+  position: absolute;
+  right: 0;
+  z-index: 1;
 `;
+const EmojiPickerButtonBlock = styled.article``;
 
 function EmojiPickerButton({ onEmojiClick, CustomButton }) {
   const [isPicking, setIsPicking] = useState(false);
@@ -17,28 +24,44 @@ function EmojiPickerButton({ onEmojiClick, CustomButton }) {
   };
 
   return (
-    <EmojiPickerButtonBlock>
-      {CustomButton ? (
-        <CustomButton onClick={emojiPickerButtonOnClickHandler} />
-      ) : (
-        <button onClick={emojiPickerButtonOnClickHandler}>추가</button>
-      )}
-      {isPicking && (
-        <EmojiPicker
-          onEmojiClick={onEmojiClickHandler}
-          width="300px"
-          height="400px"
-          skinTonesDisabled={true}
-          previewConfig={{ showPreview: false }}
-          categories={[
-            {
-              category: "smileys_people",
-              name: "Faces",
-            },
-          ]}
-        />
-      )}
-    </EmojiPickerButtonBlock>
+    <>
+      <EmojiPickerButtonBlock>
+        {
+          <ButtonStyle
+            $outlined="outlined"
+            fontSize="fontSize16"
+            $borderRadius="borderRadius6"
+            size="medium"
+            $hover="outlined"
+            $active="outlined"
+            $focus="outlined"
+            $icon
+            onClick = {emojiPickerButtonOnClickHandler}
+          >
+            <AddIcon $icon />
+            추가
+          </ButtonStyle>
+        }
+      </EmojiPickerButtonBlock>
+
+      <EmojiPickerBlock>
+        {isPicking && (
+          <EmojiPicker
+            onEmojiClick={onEmojiClickHandler}
+            width="300px"
+            height="400px"
+            skinTonesDisabled={true}
+            previewConfig={{ showPreview: false }}
+            categories={[
+              {
+                category: "smileys_people",
+                name: "Faces",
+              },
+            ]}
+          />
+        )}
+      </EmojiPickerBlock>
+    </>
   );
 }
 
